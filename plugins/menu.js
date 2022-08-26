ae7f76416d8a47ec76a5a1b6aee7dc092ae77a3f
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-08-26 11:58:26 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,79 +1,68 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let path = require('path')
+let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 let levelling = require('../lib/levelling')
 let tags = {
-  'rpgabsen': '𝐑𝐏𝐆-𝐀𝐁𝐒𝐄𝐍',
-  'rpg': '𝐑𝐏𝐆',
-  'game': '𝐆𝐀𝐌𝐄',
-  'xp': '𝐄𝐗𝐏, 𝐋𝐈𝐌𝐈𝐓',
-  'asupan': '𝐀𝐒𝐔𝐏𝐀𝐍',
-  'sticker': '𝐒𝐓𝐈𝐊𝐄𝐑',
-  'main': '𝐌𝐀𝐈𝐍',
-  'kerang': '𝐊𝐄𝐑𝐀𝐍𝐆 𝐀𝐉𝐀𝐈𝐁',
-  'quotes': '𝐐𝐔𝐎𝐓𝐄𝐒',
-  'admin': '𝐀𝐃𝐌𝐈𝐍',
-  'group': '𝐆𝐑𝐔𝐏',
-  'internet': '𝐈𝐍𝐓𝐄𝐑𝐍𝐄𝐓',
-  'anonymous': '𝐀𝐍𝐎𝐍𝐘𝐌𝐎𝐔𝐒 𝐂𝐇𝐀𝐓',
-  'downloader': '𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑',
-  'berita': '𝐁𝐄𝐑𝐈𝐓𝐀',
-  'tools': '𝐓𝐎𝐎𝐋𝐒',
-  'fun': '𝐅𝐔𝐍',
-  'database': '𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄', 
-  'vote': '𝐕𝐎𝐓𝐈𝐍𝐆',
-  'absen': '𝐀𝐁𝐒𝐄𝐍',
-  'catatan': '𝐂𝐀𝐓𝐀𝐓𝐀𝐍',
-  'jadian': '𝐉𝐀𝐃𝐈𝐀𝐍',
-  'islami': '𝐈𝐒𝐋𝐀𝐌𝐈',
-  'owner': '𝐎𝐖𝐍𝐄𝐑',
-  'virtex': '𝗩𝗜𝗥𝗧𝗘𝗫',
-  'info': '𝐈𝐍𝐅𝐎',
-  'audio': '𝐀𝐔𝐃𝐈𝐎',
-  'maker': '𝐌𝐀𝐊𝐄𝐑',
+  'rpgabsen': 'Rpg-Absen',
+  'rpg': 'Rpg',
+  'game': 'Game',
+  'xp': 'Exp, Limit & Pay',
+  'sticker': 'Sticker',
+  'main': 'Main',
+  'kerang': 'Kerang Ajaib',
+  'quotes': 'Quotes',
+  'admin': 'Admin',
+  'group': 'Group',
+  'internet': 'Internet',
+  'anonymous': 'Anonymous Chat',
+  'downloader': 'Downloader',
+  'berita': 'Berita',
+  'tools': 'Tools',
+  'fun': 'Fun',
+  'database': 'Database', 
+  'vote': 'Voting',
+  'absen': 'Absen',
+  'catatan': 'Catatan',
+  'jadian': 'Jadian',
+  'islami': 'Islami',
+  'owner': 'Owner',
+  'advanced': 'Advanced',
+  'info': 'Info',
+  'audio': 'Audio',
+  'maker': 'Maker',
 }
 const defaultMenu = {
   before: `
-❂═══❖•ೋ•:･ﾟ✧:･ﾟ✧
-╟ꂑ 𝐻𝑎𝑖, %ucapan %name! 👋
-┗━━━•❅•°•❈
-╔╦══• •✠•❀彡★
-╟⌛*𝐖𝐀𝐊𝐓𝐔:* 
-╟🕐%wib WIB
-╟🕔%wita WITA
-╟🕒%wit WIT
-╟🌄 *𝐇𝐚𝐫𝐢:* %week
-╟📅 *𝐓𝐚𝐧𝐠𝐠𝐚𝐥:* %date
-╟🕔 *𝐔𝐩𝐭𝐢𝐦𝐞:* %uptime (%muptime)
-╟👨‍ *𝐒𝐚𝐫𝐚𝐧𝐠𝐁𝐨𝐭:*
-╟https://github.com/Alfarabotz567
-╚»★★ミ✧･ﾟ: ✧ﾟ･:
+Hai, %ucapan %name! 👋
+  
+*Waktu:* 
+%wib WIB
+%wita WITA
+%wit WIT
+*Hari:* %week
+*Tanggal:* %date
+*Uptime:* %uptime (%muptime)
 
-
-📊 *𝐋𝐢𝐦𝐢𝐭𝐦𝐮:* %limit
-📶 *𝐋𝐞𝐯𝐞𝐥𝐦𝐮:* %level
-🏋 *𝐄𝐱𝐩𝐦𝐮:* %exp
+*Limit:* %limit
+*Level:* %level
+*XP:* %exp
 %readmore`.trimStart(),
-  header: '⃝▣──「 %category 」───⬣',
-  body: '│ ○ %cmd %islimit %isPremium',
-  footer: '▣────────────⬣\n',
-  after: `*Recode by*
-*Irfaan Official* | %version
+  header: ' *%category*',
+  body: ' • %cmd %islimit %isPremium',
+  footer: '\n',
+  after: `*Made by Irfaan Official*
+*%npmname* | %version
 ${'```%npmdesc```'}
 `,
 }
 let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
     let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender
-    let user = global.db.data.users[who]
-    let { exp, limit, level, money, role } = global.db.data.users[m.sender]
+    let { exp, limit, level, role } = global.db.data.users[m.sender]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let gambar = global.media
-    let name = conn.getName(m.sender)
+    let name = await conn.getName(m.sender)
     let d = new Date(new Date + 3600000)
     let locale = 'id'
     // d.getTimeZoneOffset()
@@ -111,7 +100,6 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     let muptime = clockString(_muptime)
     let uptime = clockString(_uptime)
-    
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
     let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
@@ -141,8 +129,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
+                .replace(/%islimit/g, menu.limit ? '(Ⓛ)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '')
                 .trim()
             }).join('\n')
           }),
@@ -155,8 +143,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     let replace = {
       '%': '%',
       p: _p, uptime, muptime,
+      me: conn.getName(conn.user.jid),
       ucapan: ucapan(),
-      me: conn.user.name,
       npmname: package.name,
       npmdesc: package.description,
       version: package.version,
@@ -165,60 +153,52 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       totalexp: exp,
       xp4levelup: max - exp,
       github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
-      level, limit, money, name, gambar, weton, week, date, wib, wit, wita, dateIslamic, time, totalreg, rtotalreg, role,
+      level, limit, name, weton, week, date, dateIslamic, wib, wit, wita, time, totalreg, rtotalreg, role,
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-     let audio = `https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane22.mp3`
+    let audio = `https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane22.mp3`
     await conn.sendFile(m.chat, audio, 'error.mp3', null, m, true)
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: text.trim(),
-           hydratedFooterText: wm,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '✏️ Yt Chanel',
-               url: 'https://youtube.com/channel/UCaFh82MyrVgcgIvJxvTA39w'
-             }
-
-           },
-             {
-             callButton: {
-               displayText: 'Hp Owner',
-               PhoneNumber: '0856-2482-3115'
-             }
-
-           },
-               {
-             quickReplyButton: {
-               displayText: '👤Owner',
-               id: '.owner',
-             }
-
-           },
-               {
-             quickReplyButton: {
-               displayText: '🤝 Donasi',
-               id: '.donasi',
-             }
-
-           },
-           {
-             quickReplyButton: {
-               displayText: '📳SewaBot',
-               id: '.sewa',
-             }
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
+    conn.sendHydrated(m.chat, text.trim(), 'Ⓟ premium | Ⓛ limit', null, 'https://github.com/Alfarabotz567', 'Website', '', '', [
+      ['Donate', '/donasi'],
+      ['Sewa Bot', '/sewa'],
+      ['Owner', '/owner']
+    ], m)
+    /*let url = `https://telegra.ph/file/ab1df70dfd5c2bac64da1.jpg`.trim()
+    let res = await fetch(url)
+    let buffer = await res.buffer()
+    let message = await prepareWAMessageMedia({ image: buffer }, { upload: conn.waUploadToServer })
+                const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+                    templateMessage: {
+                        hydratedTemplate: {
+                            imageMessage: message.imageMessage,
+                            hydratedContentText: text.trim(),
+                            hydratedFooterText:'Ⓟ premium | Ⓛ limit',
+                            hydratedButtons: [{
+                                urlButton: {
+                                    displayText: 'Website',
+                                    url: 'https://github.com/Alfarabotz567'
+                                }
+                            }, {
+                                quickReplyButton: {
+                                    displayText: 'Donasi',
+                                    id: '/donasi'
+                                }
+                            }, {
+                                quickReplyButton: {
+                                    displayText: 'Sewa',
+                                    id: '/sewa'
+                                }  
+                            }, {
+                                quickReplyButton: {
+                                    displayText: 'Owner',
+                                    id: '/owner'
+                                }
+                            }]
+                        }
+                    }
+                }), { userJid: m.chat, quoted: m })
+                conn.relayMessage(m.chat, template.message, { messageId: template.key.id })*/
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
@@ -226,17 +206,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 }
 handler.help = ['menu']
 handler.tags = ['main']
-handler.command = /^(menu)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.command = /^(menu|help|\?)$/i
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 handler.exp = 3
 
 module.exports = handler
@@ -250,6 +221,7 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
+
 function ucapan() {
         const hour_now = moment.tz('Asia/Jakarta').format('HH')
         var ucapanWaktu = 'Pagi kak'
