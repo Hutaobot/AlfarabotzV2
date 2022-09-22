25a2d595d1007d98553faf38196bc40fa1ccc982
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-23 03:42:06 +0700

    Delete asupan-malaysia.js

--- plugins/asupan-malaysia.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-let babi = 'https://api.lolhuman.xyz/api/asupan?apikey=cd71d6b3f5c1352f55a46883'
-    conn.sendButtonImg(m.chat, babi, 'Nih', wm2, 'NEXT', '.malaysia', m)
-}
-handler.help = ['asupan']
-handler.tags = ['asupan']
-handler.command = /^(asupan)$/i
-
-module.exports = handler
