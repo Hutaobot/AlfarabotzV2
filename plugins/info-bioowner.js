789b351e9ee7f7531e3245b41530118e974568db
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-23 05:23:01 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,25 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Irfaan Official
-*✉️ Nama RL* : Irfaan Official
+*💌 Nama* : A7 RAFLI 
+*✉️ Nama RL* : M.Rafli Setiawan
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot, Suka Adzan di masjid
-*💬 Sifat* : Baik, Ramah, Kanjut, Prik, Suka berbagi 
-*🗺️ Tinggal* : Indonesia, Tasikmalaya, Singaparna, Mangkubumi
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*⏰ Tanggal lahir* : 14 Juli 2006
+*🎨 Umur* : 16
+*🧮 Kelas* : 2 SMA 
+*🧩 Hobby* : Nonton Anime, Chatting, Recode Script Bot, Nonton Hentai
+*💬 Sifat* : Baik, Toxic, Ramah, Suka Berbagi, Friendly, Kurang Bergaul 
+*🗺️ Tinggal* : Indonesia, Jawa Barat, Sukabumi, Kabandungan, Mekarjaya 
+*❤️ Suka* :  Anime, Waifu, Loli, Trap, Furry, Kucing, Hentai, 
+*💔 Benci* : Autis, Seleb, Cewe Sasimo, Rasis
+*❤️ Pacar* : Gak punya🙂
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @Irfaanabdulhafiizh
-*🇫  Facebook* : Irfaan Abdul Hafiizh
-*🏮 Chanel Youtube* : HimHia Channel
-*🐈 Github:* Alfarabotz567
+*📷 instagran* : @rafli_editz2
+*🍻 TikTok* : rafli_editz2
+*🏮 Chanel Youtube* : A7 RAFLI
+*🐈 Github:* Futaroukun
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
