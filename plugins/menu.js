0616fd649880def2741ee5539f7ab7d23df7ee91
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-23 06:10:53 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -52,8 +52,8 @@ Hai, %ucapan %name! 👋
   header: ' *%category*',
   body: ' • %cmd %islimit %isPremium',
   footer: '\n',
-  after: `*Made by Irfaan Official*
-*Alfarabotz* | %version
+  after: `*Made by A7 RAFLI*
+*Hu Tao Bot-MD* | %version
 ${'```%npmdesc```'}
 `,
 }
@@ -159,7 +159,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     let audio = `${pickRandom(['https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane22.mp3', 'https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane15.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane1.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane2.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane20.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane10.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane23.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane17.mp3','https://raw.githubusercontent.com/hyuura/Rest-Sound/main/HyuuraKane/mangkane21.mp3'])}`
     await conn.sendFile(m.chat, audio, 'error.mp3', null, m, true)
-    conn.sendButton(m.chat, text.trim(), 'Made by Irfaan Official', null, [['Donasi', '.donasi'],['Owner', '.owner']], m)
+    conn.sendButton(m.chat, text.trim(), 'Made by A7 RAFLI', null, [['Donasi', '.donasi'],['Owner', '.owner']], m)
     /*conn.sendHydrated(m.chat, text.trim(), 'Ⓟ premium | Ⓛ limit', null, 'https://github.com/Alfarabotz567', 'Website', '', '', [
       ['Donate', '/donasi'],
       ['Sewa Bot', '/sewa'],
@@ -178,7 +178,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
                             hydratedButtons: [{
                                 urlButton: {
                                     displayText: 'Website',
-                                    url: 'https://github.com/Alfarabotz567'
+                                    url: 'https://linktr.ee/rafli_editz2'
                                 }
                             }, {
                                 quickReplyButton: {
