5366c92c94f5f271f49d58f24ae344de8fb99ff0
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-23 04:24:21 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -13,15 +13,15 @@ let handler = async (m, { conn }) => {
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15.000 Dana
+Rp. 10.000 Dana
 Rp. 15.000 Pulsa
 
 2. Premium / 30 Hari
 Rp. 10.000 Dana
-Rp. 10.000 Pulsa
+Rp. 15.000 Pulsa
 
 3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
+Rp. 15.000 Dana
 Rp. 20.000 Pulsa
 
 wa.me/${owner[0]}
@@ -36,9 +36,9 @@ wa.me/${owner[0]}
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: `6285624823115@s.whatsapp.net`
+  businessOwnerJid: `6283854551575@s.whatsapp.net`
   },
-  footerText: 'https://github.com/Alfarabotz567',
+  footerText: 'https://github.com/Futaroukun',
   }},{})
   conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
