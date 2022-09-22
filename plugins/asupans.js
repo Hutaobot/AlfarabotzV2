96869c829aae8cce7cb5b6017b6d7502b4360d06
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-23 03:45:50 +0700

    Update asupans.js

--- plugins/asupans.js
+++ plugins/asupans.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', 'awas Sange Bang😅', m)
 }
 handler.help = ['asupan']
-handler.tags = ['asupan']
+handler.tags = ['fun']
 
 handler.command = /^(asupan)$/i
 handler.premium = false
