6cb8562b4909bdadf0d09672f84f9ee21cc57d8e
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-24 04:33:46 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,6 +1,8 @@
 let handler = m => m.reply('Hai kak lagi nyari sc yaa? \n\n\n_*https://wa.me/6283854551575?text=Assalamualaikum%20Bang%20bagi%20sc%20nya%20dong?*_')
 
-handler.customPrefix = /sc|Sc|Script|script/i
+handler.customPrefix = /Sc|sc|script/i
 handler.command = new RegExp
+handler.premium = true
+handler.register = true
 
 module.exports = handler
